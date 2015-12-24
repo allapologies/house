b3194c5bb37b1f648ef4e91717c57f956d010528
aleksandr <alx.panteleev@gmail.com>
2015-12-24 13:01:45 +0300

    Post handler

--- routes/calculator.js
+++ routes/calculator.js
@@ -5,4 +5,15 @@ router.get('/', function(req, res, next) {
   res.render('calculator', { title: 'Калькулятор' });
 });
 
+router.post('/', function(req, res, next) {
+  var house = {
+    rooms : parseInt(req.body.rooms) || 3,
+    floors : parseInt(req.body.floors) || 1,
+    roof : req.body.roof || "flat",
+    ceiling : parseInt(req.body.ceiling, 10) || 2.7 ,
+    foundation : req.body.foundation || "lenta"
+  };
+  res.json(house);
+});
+
 module.exports = router;
