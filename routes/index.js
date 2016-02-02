67aab3dca89e3d1b05583026bbc1a303130d5ac7
aleksandr <alx.panteleev@gmail.com>
2016-02-02 21:52:56 +0300

    unstable

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var helpers = require('../handlers/helpers.js');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Расчёт строительства дома Online' });
+
+  fs.readFile('public/templates/bootstrap.html', function(err, contents){
+    if (err) {
+      helpers.send_failure(res, 500, err);
+      return;
+    };
+
+    contents = contents.toString();
+    contents = contents.replace('{{PAGE_NAME}}', 'home');
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(contents);
+  });
+});
+
+router.get('/home.json', function(req, res, next) {
+
+  fs.readFile('public/templates/home.html', function(err, contents){
+    if (err) {
+      helpers.send_failure(res, 500, err);
+      return;
+    };
+    contents = contents.toString();
+    contents = {
+      "slider":"<p>THIS IS SLIDER</p>",
+      "content":"<p>THIS IS CONTENT</p>"
+    };
+
+    var response = {"error": "null", "data": contents};
+    res.json(response);
+  });
+
+
 });
 
 module.exports = router;
