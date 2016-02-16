d3587ccf2443e34d259c306774d2d5db464ce256
aleksandr <alx.panteleev@gmail.com>
2016-02-16 20:55:55 +0300

    missing templates and directory

---
+++ public/javascripts/app/directory.js
@@ -0,0 +1,51 @@
+define({
+  beton:{
+    name:"Бетон",
+    unit: "м3"
+  },
+  metall:{
+    name:"Арматура",
+    unit: "пог.м"
+  },
+  bricks:{
+    name:"Газоблоки",
+    unit: "м3"
+  },
+  glue:{
+    name:"Клей для газоблоков",
+    unit: "кг"
+  },
+  foamPlastic:{
+    name:"Пенополистирол",
+    unit: "м3"
+  },
+  plaster:{
+    name:"Штукатурка",
+    unit: "кг"
+  },
+  svai:{
+    name:"Винтовые сваи",
+    unit: "шт"
+  },
+  wood:{
+    name:"Доска деревянная",
+    unit: "м3"
+  },
+  ruberoid:{
+    name:"Руберойд",
+    unit: "м2"
+  },
+  tile:{
+    name:"Черепица",
+    unit: "м2"
+  },
+  foundation:{
+    name:"Фундамент"
+  },
+  roof:{
+    name:"Кровля"
+  },
+  walls:{
+    name:"Стены"
+  }
+});
