f3bb59f7f705b5b800b0990c5434b15363b27a88
Aleksandr <alx.panteleev@gmail.com>
2016-05-18 18:15:09 +0300

    HOC impemented

---
+++ src/HOC/onInputChange.js
@@ -0,0 +1,24 @@
+'use strict';
+import React, { Component, PropTypes } from 'react';
+
+export default function onInputChange (CustomComponent) {
+
+  return class extends Component {
+
+    render() {
+      return <CustomComponent
+        {...this.state}
+        {...this.props}
+        {...{onInputChange: this.onInputChange}}
+      />
+    }
+
+    onInputChange = (event)=> {
+      let nextState = {};
+      nextState[event.target.name] = event.target.value;
+      console.log('HOC!!!!', nextState);
+      this.setState(nextState);
+    };
+  }
+}
+
