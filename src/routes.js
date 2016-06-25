487712cc54fa261c0f45eb3f1a0e06888d5c5be2
Aleksandr <alx.panteleev@gmail.com>
2016-06-25 16:11:54 +0300

    Removing login functionality

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Route , IndexRoute, IndexRedirect } from 'react-router';
+import { Route , IndexRoute } from 'react-router';
 import SpendsInput from './containers/NewSpends';
 import App from './components/app';
 import ProjectList from './containers/ProjectsList';
 import NewProject from './components/NewProject';
 import ProjectDescription from './containers/ProjectDescription';
 import NotFound from './components/NotFound';
-import LoginPage from './containers/LoginPage';
 
 export default (
   <Route path='/' component={App}>
-    <IndexRoute component={LoginPage} />
+    <IndexRoute component={ProjectList} />
     <Route path='projects'>
-      <IndexRoute component={ProjectList} />
       <Route path='new' component={NewProject} />
           <Route path=':id' component={ProjectDescription} />
           <Route path=':id/spendings/new' component={SpendsInput} />
