bf0e99e18de7dfe41c7122b73eb9aea331aaccb5
Aleksandr <alx.panteleev@gmail.com>
2016-07-03 11:41:46 +0300

    spends input via redux-form

--- src/components/SpendsInput.js
+++ src/components/SpendsInput.js
@@ -1,6 +1,6 @@
 'use strict';
 import React, { Component, PropTypes } from 'react';
-import onInputChange from '../HOC/onInputChange';
+import {reduxForm} from 'redux-form';
 
 class SpendsInput extends Component {
   constructor(props) {
@@ -36,6 +36,10 @@ class SpendsInput extends Component {
   };
 
   render() {
+    const { fields: {
+        stage, subStage, material, supplier,
+        quantity, unit, price, comments
+    } } = this.props
     return (
       <div className='row'>
         <div className='col-sm-6 col-md-8 col-sm-offset-3 col-md-offset-2'>
@@ -45,9 +49,7 @@ class SpendsInput extends Component {
             <div className='form-group'>
               <select
                 className='form-control'
-                name='stage'
-                value={this.state.stage}
-                onChange={this.props.onInputChange}>
+                { ... stage}>
                 <option value={0} disabled>Категория</option>
                 {this.getOptions('stages')}
               </select>
@@ -55,9 +57,7 @@ class SpendsInput extends Component {
             <div className='form-group'>
               <select
                 className='form-control'
-                name='subStage'
-                value={this.state.subStage}
-                onChange={this.props.onInputChange}>
+                 {...subStage}>
                 <option value={0} disabled>Подкатегория</option>
                 {this.getOptions('subStages')}
               </select>
@@ -65,9 +65,7 @@ class SpendsInput extends Component {
             <div className='form-group'>
               <select
                 className='form-control'
-                name='material'
-                value={this.state.material}
-                onChange={this.props.onInputChange}>
+                {...material}>
                 <option value={0} disabled>Материалы</option>
                 {this.getOptions('materials')}
               </select>
@@ -75,9 +73,7 @@ class SpendsInput extends Component {
             <div className='form-group'>
               <select
                 className='form-control'
-                name='suppliers'
-                value={this.state.supplier}
-                onChange={this.props.onInputChange}>
+                {...supplier}>
                 <option value={0}>Поставщик</option>
                 <option value="1">Первый</option>
                 <option value="1">Второй</option>
@@ -86,18 +82,13 @@ class SpendsInput extends Component {
             <div className='form-group'>
               <input
                 type='text'
-                name='quantity'
                 className='form-control'
-                placeholder='количество'
-                value={this.state.quantity}
-                onChange={this.props.onInputChange}/>
+                {...quantity} />
             </div>
             <div className='form-group'>
               <select
                 className='form-control'
-                name='unit'
-                value={this.state.unit}
-                onChange={this.props.onInputChange}>
+                {...unit}>
                 <option value={0} disabled>Ед.изм</option>
                 {this.getOptions('units')}
               </select>
@@ -105,20 +96,15 @@ class SpendsInput extends Component {
             <div className='form-group'>
               <input
                 type='text'
-                name='price'
                 className='form-control'
-                placeholder='цена'
-                value={this.state.price}
-                onChange={this.props.onInputChange}/>
+                {...price}
+                />
             </div>
             <div className='form-group'>
               <textarea
               className='form-control'
-              name='comments'
               rows="3"
-              placeholder='комментарии'
-              value={this.state.comments}
-              onChange={this.props.onInputChange}/>
+              {...comments}/>
             </div>
             <div className='form-group'>
               <button type='submit' className='btn btn-default'>Сохранить</button>
@@ -130,4 +116,7 @@ class SpendsInput extends Component {
   }
 }
 
-export default onInputChange(SpendsInput);
+export default reduxForm({
+    form: 'spendsInput',
+    fields: ['stage', 'subStage', 'material', 'supplier', 'quantity', 'unit', 'price', 'comments']
+})(SpendsInput);
