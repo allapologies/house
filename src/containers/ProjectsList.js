994765c6dfb92acd8b4751d075d1e755853e1cc4
Aleksandr <alx.panteleev@gmail.com>
2016-07-26 20:21:02 +0300

    refactor projects List

--- src/containers/ProjectsList.js
+++ src/containers/ProjectsList.js
@@ -1,47 +1,52 @@
-'use strict';
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+'use strict' 
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import ProjectsListItem from '../components/ProjectsListItem';
-import { fetchProjects } from '../actions';
+import ProjectsListItem from '../components/ProjectsListItem' 
+import { fetchProjects } from '../actions'
 
 
 class ProjectsList extends Component {
-  componentWillMount() {
-    this.props.fetchProjects();
-  }
-  
-  render() {
-    if (!this.props.projects.get('all')) {
-      return <div>Пожалуйста, подождите. Идёт загрузка</div>
+    static propTypes = {
+        projects: React.PropTypes.object.isRequired,
+        fetchProjects: React.PropTypes.func
     }
 
-    const projects = this.props.projects.get('all').map( (project) => {
-      return <ProjectsListItem  key={project.projectId} project={project} />
-    });
+    componentWillMount() {
+        this.props.fetchProjects() 
+    }
 
-    return (
-      <div className='projectList row'>
-        <div className='col-sm-6 col-sm-offset-3'>
-          <ul className='list-group'>
-            {projects}
-          </ul>
-          <Link to='/projects/new' className='btn btn-default'>
-            Создать проект
-          </Link>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const projects = this.props.projects.get('items')
+        if (!projects) {
+            return <div>Пожалуйста, подождите. Идёт загрузка</div>
+        }
+        const projectsList = projects.map((project) => {
+            return <ProjectsListItem key={project.projectId} project={project}/>
+        }) 
+
+        return (
+            <div className='projectList row'>
+                <div className='col-sm-6 col-sm-offset-3'>
+                    <ul className='list-group'>
+                        {projectsList}
+                    </ul>
+                    <Link to='/projects/new' className='btn btn-default'>
+                        Создать проект
+                    </Link>
+                </div>
+            </div>
+        ) 
+    }
 }
 
 function mapStateToProps(state) {
-  return { projects: state.projects };
+    return {projects: state.projects} 
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchProjects }, dispatch);
+    return bindActionCreators({fetchProjects}, dispatch) 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectsList);
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsList) 
