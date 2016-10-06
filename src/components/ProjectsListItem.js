c9342c07e3baf8f4a75eb65699dd73a7380f9bfe
aleksandr <alx.panteleev@gmail.com>
2016-10-06 22:58:51 +0300

    fix prop validation

--- src/components/ProjectsListItem.js
+++ src/components/ProjectsListItem.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'react-router'
 
-export default (props) => {
-  const { projectId, title} = props.project;
-  const url = `/projects/${projectId}`;
-  return (
-      <li className='list-group-item'>
+export const ProjectsListItem = (props) => {
+    const { projectId, title } = props.project
+    const url = `/projects/${projectId}`
+
+    return (
+      <li className="list-group-item">
         <Link to={url}>{title}</Link>
       </li>
-    );
+    )
+}
+
+ProjectsListItem.propTypes = {
+    project: React.PropTypes.shape({
+        projectId: React.PropTypes.number.isRequired,
+        title: React.PropTypes.string.isRequired
+    })
 }
